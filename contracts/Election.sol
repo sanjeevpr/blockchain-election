4f9c98da36d4c3d0693159397f5f9364cf3695c1
Sanjeev <sanjiv.pradhan94@gmail.com>
2021-05-25 16:06:05 +0530

    Cast Votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,33 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Election {
-    string public candidate;
 
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidates("Oarack Bobama");
+        addCandidates("Tonald Drump");
+    }
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
+
+    uint public candidatesCount;
+
+    function addCandidates(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender]);
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
     }
 }
---
+++ test/election.js
@@ -0,0 +1,86 @@
+var Election = artifacts.require("./Election.sol");
+
+contract("Election", function(accounts) {
+
+    var election;
+
+    it("initializes with two candidates", function() {
+        return Election.deployed().then(function(instance) {
+            return instance.candidatesCount();
+        }).then(function(count) {
+            assert.equal(count, 2);
+        })
+    })
+
+    it("initializes candidates with correct values", function() {
+        return Election.deployed().then(function(instance) {
+            election = instance;
+            return election.candidates(1);
+        }).then(function(candidate) {
+            assert.equal(candidate[0], 1, "contains the correct id");
+            assert.equal(candidate[1], "Oarack Bobama", "contains the correct name");
+            assert.equal(candidate[2], 0, "contains the correct votes count");
+            return election.candidates(2);
+        }).then(function(candidate) {
+            assert.equal(candidate[0], 2, "contains the correct id");
+            assert.equal(candidate[1], "Tonald Drump", "contains the correct name");
+            assert.equal(candidate[2], 0, "contains the correct votes count");
+        });
+    });
+
+    it("allows a voter to cast the vote", function() {
+        return Election.deployed().then(function(instance) {
+            election = instance;
+            candidateId = 1;
+            return election.vote(candidateId, { from: accounts[0] });
+        }).then(function(receipt) {
+            return election.voters(accounts[0]);
+        }).then(function(voted) {
+            assert(voted, "the voter was marked as voted");
+            return election.candidates(candidateId);
+        }).then(function(candidate) {
+            var voteCount = candidate[2];
+            assert.equal(voteCount, 1, "increments the candidate's vote count");
+        })
+    });
+
+    it("throws an exception for invalid candidate", function() {
+        return Election.deployed().then(function(instance) {
+            election = instance;
+            return election.vote(99, { from: accounts[0] });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf("revert") >= 0, "error message must contain revert")
+            return election.candidates(1);
+        }).then(function(candidate1) {
+            var voteCount = candidate1[2];
+            assert.equal(1, voteCount, "candidate 1 didn't received any votes");
+            return election.candidates(2);
+        }).then(function(candidate2) {
+            var voteCount = candidate2[2];
+            assert.equal(0, voteCount, "candidate 2 didn't received any votes");
+        })
+    });
+
+    it("throws an exception for double voting", function() {
+        return Election.deployed().then(function(instance) {
+            election = instance;
+            candidateId = 2;
+            election.vote(candidateId, { from : accounts[1] });
+            return election.candidates(candidateId);
+        }).then(function(candidate) {
+            var voteCount = candidate[2];
+            assert.equal(voteCount, 1, "accepts the first vote");
+            return election.vote(candidateId, { from : accounts[1] });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf("revert") >= 0, "error message must contain revert");
+            return election.candidates(1);
+        }).then(function(candidate1) {
+            var voteCount = candidate1[2];
+            assert.equal(voteCount, 1, "candidate 1 didn't received any votes");
+            return election.candidates(2);
+        }).then(function(candidate2) {
+            var voteCount = candidate2[2];
+            assert.equal(voteCount, 1, "candidate 2 didn't received any votes");
+        })
+    });
+})
